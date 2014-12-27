7b2b75643c14e9ba3b706c61039d337b125867bf
Imad Rajab <imadrajab@gmail.com>
2014-12-27 18:47:19 +0200

    fixing metabox

--- vboutwp/includes/meta_box.php
+++ vboutwp/includes/meta_box.php
@@ -5,14 +5,22 @@ global $post_id;
 $post_type = get_post_type();
 $post_list = array(); //$this->get_post_list( $post_type, $post_id );
 
-$hasp_expire_enable = get_post_meta( $post_id, 'hasp_expire_enable', true );
-$hasp_expire_date = get_post_meta( $post_id, 'hasp_expire_date', true ) ? date( 'Y-m-d H:i', strtotime( get_post_meta( $post_id, 'hasp_expire_date', true ) ) ) : null;
-$hasp_overwrite_enable = get_post_meta( $post_id, 'hasp_overwrite_enable', true );
-$hasp_overwrite_post_id = get_post_meta( $post_id, 'hasp_overwrite_post_id', true );
+
+///	ADD META POST/PAGE TO CHECK IF THIS POST/PAGE IS ALREADY SENT TO VBOUT
+//$hasp_expire_enable = get_post_meta( $post_id, 'hasp_expire_enable', true );
 
 ?>
 
-<p><label><input type="checkbox" name="hasp_expire_enable" id="hasp_expire_enable" <?php if( $hasp_expire_enable == 1 ) echo 'checked="checked"'; ?>><?php _e( 'Datetime of expiration', 'hasp' ) ?></label></p>
+<div id="vbout_post_to_channels_box" class="postbox">
+	<h3>
+		<input type="checkbox" name="vb_post_to_channels" id="vb_post_to_channels" /><?php _e( 'Post to social channels?', 'vblng' ); ?>
+	</h3>
+
+	<div class="inside">
+		here...
+	</div>
+</div>
+
 
 <div id="hasp_expire_div" style="display: none;">
 
