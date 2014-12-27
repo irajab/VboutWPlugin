9a6360813bfa9d1cdb8dd9bc33af60f122edf868
Imad Rajab <imadrajab@gmail.com>
2014-12-27 18:47:49 +0200

    fixing metabox 2

--- vboutwp/includes/meta_box.php
+++ vboutwp/includes/meta_box.php
@@ -13,33 +13,112 @@ $post_list = array(); //$this->get_post_list( $post_type, $post_id );
 
 <div id="vbout_post_to_channels_box" class="postbox">
 	<h3>
-		<input type="checkbox" name="vb_post_to_channels" id="vb_post_to_channels" /><?php _e( 'Post to social channels?', 'vblng' ); ?>
+		<span>
+			<label><input type="checkbox" name="vb_post_to_channels" id="vb_post_to_channels" /><?php _e( 'Post to social channels?', 'vblng' ); ?></label>
+		</span>
 	</h3>
 
 	<div class="inside">
-		here...
+		<p>Please choose which social channel you want to post to:</p>
+		<table width="100%" cellspacing="0" cellpadding="0">
+			<tr>
+				<td>Facebook:</td>
+				<td>- NONE -</td>
+			</tr>
+			<tr>
+				<td>Twitter:</td>
+				<td>- NONE -</td>
+			</tr>
+			<tr>
+				<td>Linkedin:</td>
+				<td>- NONE -</td>
+			</tr>
+		</table>
 	</div>
 </div>
 
+<div id="vbout_post_to_campaign_box" class="postbox">
+	<h3>
+		<span>
+			<label><input type="checkbox" name="vb_post_to_campaign" id="vb_post_to_campaign" /><?php _e( 'Post as campaign?', 'vblng' ); ?></label>
+		</span>
+	</h3>
 
-<div id="hasp_expire_div" style="display: none;">
-
-	<span><input type="text" id="hasp_expire_date" name="hasp_expire_date" size="13" placeholder="<?php _e( 'Y-m-d H:i', 'hasp' ) ?>" value="<?php echo $hasp_expire_date ?>"></span>
-	<span id="hasp_expire_error_1" class="hasp_error_mes" style="display: none"><?php _e( 'Input datetime.', 'hasp' ) ?></span>
-	<span id="hasp_expire_error_2" class="hasp_error_mes" style="display: none"><?php _e( 'Input future datetime.', 'hasp' ) ?></span>
-	
+	<div class="inside">
+		<p>Please choose which lists you want to post to:</p>
+		<span>- NONE -</span>
+		<table class="form-table">
+			<tr valign="top">
+				<th scope="row" style="width: auto;">
+					<label for="vb_post_schedule_emailsubject"><?php _e( 'Email Subject', 'vblng' ); ?></label>
+				</th>
+				<td>
+					<input type="text" name="vb_post_schedule_emailsubject" id="vb_post_schedule_emailsubject" value="" class="" />
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row" style="width: auto;">
+					<label for="vb_post_schedule_fromemail"><?php _e( 'From Email', 'vblng' ); ?></label>
+				</th>
+				<td>
+					<input type="text" name="vb_post_schedule_fromemail" id="vb_post_schedule_fromemail" value="" class="" />
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row" style="width: auto;">
+					<label for="vb_post_schedule_fromname"><?php _e( 'From Name', 'vblng' ); ?></label>
+				</th>
+				<td>
+					<input type="text" name="vb_post_schedule_fromname" id="vb_post_schedule_fromname" value="" class="" />
+				</td>
+			</tr>
+			<tr valign="top">
+				<th scope="row" style="width: auto;">
+					<label for="vb_post_schedule_replyto"><?php _e( 'Reply to', 'vblng' ); ?></label>
+				</th>
+				<td>
+					<input type="text" name="vb_post_schedule_replyto" id="vb_post_schedule_replyto" value="" class="" />
+				</td>
+			</tr>
+		</table>
+	</div>
 </div>
 
-<p><label><input type="checkbox" name="hasp_overwrite_enable" id="hasp_overwrite_enable" <?php if( $hasp_overwrite_enable == 1 ) echo 'checked="checked"'; ?>><?php _e( 'Overwrite the another post', 'hasp' ) ?></label></p>
-
-<div id="hasp_overwrite_div" style="display: none;">
-
-	<select name="hasp_overwrite_post_id" id="hasp_overwrite_post_id">
-		<option value="0">— <?php _e( 'Select' ) ?> —</option>
-		<?php foreach( $post_list as $post ) : ?>
-			<option value="<?php echo $post->ID ?>" <?php if( $hasp_overwrite_post_id == $post->ID ) echo 'selected'?>><?php echo $post->post_title ?></option>
-		<?php endforeach; ?>
-	</select>
-	<span id="hasp_overwrite_error" class="hasp_error_mes" style="display: none"><?php _e( 'Select the post.', 'hasp' ) ?></span>
-	<p><?php _e( 'You can set schedule which overwrites the another post.', 'hasp' ) ?></p>
+<div>
+	<table class="form-table">
+		<tr valign="top">
+			<th scope="row" style="width: auto;">
+				<label for="vb_post_schedule_date"><?php _e( 'Schedule Date', 'vblng' ); ?></label>
+			</th>
+			<td>
+				<input type="text" name="vb_post_schedule_date" id="vb_post_schedule_date" value="" class="" />
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row" style="width: auto;">
+				<label for="vb_post_schedule_hours"><?php _e( 'Hours', 'vblng' ); ?></label>
+			</th>
+			<th scope="row" style="width: auto;">
+				<label for="vb_post_schedule_minutes"><?php _e( 'Minutes', 'vblng' ); ?></label>
+			</th>
+		</tr>
+		<tr valign="top">
+			<td>
+				<input type="text" name="vb_post_schedule_hours" id="vb_post_schedule_hours" value="" class="" />
+			</td>
+			<td>
+				<input type="text" name="vb_post_schedule_minutes" id="vb_post_schedule_minutes" value="" class="" />
+			</td>
+		</tr>
+		<tr valign="top">
+			<th scope="row">
+				<label for="vb_post_schedule_shortenurls"><?php _e( 'Use shorten URLs', 'vblng' ); ?></label>
+			</th>
+			<td>
+				<label for="vb_post_schedule_shortenurls">
+					<input type="checkbox" name="vb_post_schedule_shortenurls" id="vb_post_schedule_shortenurls" value="yes" />
+				</label>
+			</td>
+		</tr>
+	</table>
 </div>
