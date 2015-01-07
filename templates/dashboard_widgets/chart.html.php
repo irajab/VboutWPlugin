c42cd96573398dc5c594dc1b2f01d58d806fff8b
Imad Rajab <imadrajab@gmail.com>
2015-01-07 18:19:18 +0200

    deleted unused files

--- templates/dashboard_widgets/chart.html.php
+++
@@ -1,4 +0,0 @@
-<div class="SpamHammerServiceStats" align="center">
-	<div id="SpamHammerReportsChart" style="<?php global $wp_version; if (version_compare($wp_version, "3.8", "<")): ?>background-color: white;<?php endif; ?> height: 200px;"></div>
-	<div class="Motd"></div>
-</div>
--- templates/dashboard_widgets/counter.html.php
+++
@@ -1,8 +0,0 @@
-<div class="SpamHammerServiceStats" align="center">
-	<div>
-		<div class="Attacks SexText"><?php echo __('Counting', 'spam-hammer'); ?></div>
-	</div>
-	<?php if ($description): ?>
-		<div><?php echo $description; ?></div>
-	<?php endif; ?>
-</div>
